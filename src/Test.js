9bbff53815ee5e99aef20f325460694ddc3f3837
Adil Naseem <adil.naseem@lyftyfy.com>
2023-09-20 23:10:07 +0500

    app memo version added

--- src/Test.js
+++ src/Test.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 function SlowComponent() {
   // If this is too slow on your maching, reduce the `length`
   const words = Array.from({ length: 100_000 }, () => "WORD");
+  console.log("Slow component called");
   return (
     <ul>
       {words.map((word, i) => (
@@ -14,13 +15,36 @@ function SlowComponent() {
   );
 }
 
-export default function Test() {
+function Counter({ children }) {
   const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+      {children}
     </div>
+  );
+}
+
+export default function Test() {
+  // slow component is called each time
+  // const [count, setCount] = useState(0);
+  // return (
+  //   <div>
+  //     <h1>Slow counter?!?</h1>
+  //     <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+  //     <SlowComponent />
+  //   </div>
+  // );
+
+  // slow component is called only once when creating Counter
+  // state updation in Counter do not cause Slow component to render again.
+  // Just counter runs again
+  return (
+    <>
+      <Counter>
+        <SlowComponent />
+      </Counter>
+    </>
   );
 }
